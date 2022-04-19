112a25a8334667ac8cf7ad1515a83082e23a7ef4
Canberk <canberkkaya06@gmail.com>
2022-04-19 14:48:16 +0300

    shopping cart screen and cartItem component

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, View } from 'react-native'
 import HomeScreen from './src/screens/HomeScreen'
 import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen'
 import MenuDetailsScreen from './src/screens/MenuDetailsScreen'
+import CartScreen from './src/screens/CartScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MenuDetailsScreen />
+      <CartScreen />
+      {/* <MenuDetailsScreen /> */}
       {/* <RestaurantDetailsScreen /> */}
       {/* <HomeScreen /> */}
       <StatusBar style='auto' />
